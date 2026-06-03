078edb0a895f52d745ff5e0fc9f070fb8ebbf4ff
swalihhaneef <swalihsr7l@gmail.com>
2026-06-03 20:04:19 +0530

    config added

---
+++ config.js
@@ -0,0 +1,5 @@
+    // export const API_URL = "http://localhost:4000/api/";
+    // export const BASE_URL = "http://localhost:4000";
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ||"https://api.horatio.in/api/";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ||"https://api.horatio.in";
